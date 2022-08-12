9a7a460faa0efef9f8bcf347db480ad88e5f6f5e
Burak Akkaya <akkaya.burak@outlook.com>
2022-08-12 22:28:14 +0300

    first commit

--- tft/settings.py
+++ tft/settings.py
@@ -134,6 +134,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # Enable WhiteNoise's GZip compression of static assets.
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
