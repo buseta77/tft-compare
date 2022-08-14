764ada5460532aebe76d73f631e78d07fa2bf879
Burak Akkaya <akkaya.burak@outlook.com>
2022-08-15 02:10:47 +0300

    first commit

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from backend.views import GamesTogether, Home
 
 urlpatterns = [
-    path('/', Home.as_view(), name='home'),
+    path('', Home.as_view(), name='home'),
     path('compare/', GamesTogether.as_view(), name='compare')
 ]
