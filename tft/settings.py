afc64470c00d9ca8b7522aa28d285111377f4b29
Burak Akkaya <akkaya.burak@outlook.com>
2022-08-15 17:11:38 +0300

    first commit

--- tft/settings.py
+++ tft/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 
 IS_HEROKU = "DYNO" in os.environ
@@ -140,11 +140,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
-)
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 
 # Enable WhiteNoise's GZip compression of static assets.
